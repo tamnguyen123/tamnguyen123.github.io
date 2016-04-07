5f52a8433b09ef681e8c2eebe5596d5d6f252dbb
minh tam <tamlkp2@gmail.com>
2016-04-07 11:18:43 +0700

    fix function js
    optimize code func hide()

--- html-css/baitap6/js/display.js
+++ html-css/baitap6/js/display.js
@@ -1,45 +1,49 @@
-var flag1=true;
-var flag2=true;
-var flag3=true;
-function hide1(){
-	if(flag1){
-		document.getElementById("content1").style.display = "none";
-		/*document.getElementsByClassName("close").style.background = 'url("images/top-movie-trailers-arrows.gif") no-repeat 0 -22';*/
-		document.getElementById("close1").src = "images/open-arrow.png";
-		flag1=false;
+// check open or close of content
+var flag=false;
+var flag2=false;
+var flag3=false;
+
+// func show/hide of content
+function hide(num) {
+	if(num==1) {
+		if(!flag) {
+			document.getElementById("content1").style.display = "none";
+			document.getElementById("close1").src = "images/close-arrows.png";
+			flag=true;
+		}
+		else{
+			document.getElementById("content1").style.display = "block";
+			document.getElementById("close1").src = "images/open-arrow.png";
+			flag=false;
+		}
 	}
-	else{
-		document.getElementById("content1").style.display = "block";
-		document.getElementById("close1").src = "images/close-arrows.png";
-		flag1=true;
+	else if(num==2) {
+		if(!flag2) {
+			document.getElementById("content2").style.display = "block";
+			document.getElementById("close2").src = "images/open-arrow.png";
+			flag2=true;
+		}
+		else {
+			document.getElementById("content2").style.display = "none";
+			document.getElementById("close2").src = "images/close-arrows.png";
+			
+			flag2=false;
+		}
+	}
+	else {
+		if(!flag3) {
+			document.getElementById("content3").style.display = "block";
+			document.getElementById("close3").src = "images/open-arrow.png";
+			flag3=true;
+		}
+		else {
+			document.getElementById("content3").style.display = "none";
+			document.getElementById("close3").src = "images/close-arrows.png";
+			
+			flag3=false;
+		}
 	}
 }
 
-function hide2(){
-	if(flag2){
-		document.getElementById("content2").style.display = "block";
-		document.getElementById("close2").src = "images/close-arrows.png";
-		flag2=false;
-	}
-	else{
-		
-		document.getElementById("content2").style.display = "none";
-		document.getElementById("close2").src = "images/open-arrow.png";
-		flag2=true;
-	}
-}
 
-function hide3(){
-	if(flag3){
-		document.getElementById("content3").style.display = "block";
-		document.getElementById("close3").src = "images/close-arrows.png";
-		flag3=false;
-	}
-	else{
-		
-		document.getElementById("content3").style.display = "none";
-		document.getElementById("close3").src = "images/open-arrow.png";
-		flag3=true;
-	}
-}
 
