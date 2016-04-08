13d8def98a668c0b2a442e6bf72dea244e025261
tamnguyen123 <tamlkp2@gmail.com>
2016-04-08 11:51:06 +0700

    Update display.js

--- html-css/baitap6/js/display.js
+++ html-css/baitap6/js/display.js
@@ -3,12 +3,12 @@ function showhideContent(position)
 {
     var attrdisplay=document.getElementById("content"+position).style.display;
     if(attrdisplay!="none") {
-        document.getElementById("content"+position).style.display = "none";
+        document.getElementById("content"+position).style.display="none";
         document.getElementById("close"+position).src = "images/close-arrows.png";
     }
     else {
-        document.getElementById("content"+position).style.display = "block";
-        document.getElementById("close"+position).src = "images/open-arrow.png";
+        document.getElementById("content"+position).style.display="block";
+        document.getElementById("close"+position).src="images/open-arrow.png";
     }
 }	
 
