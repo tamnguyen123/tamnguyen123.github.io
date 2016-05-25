e521b6165cc6f84727fc0db9c75765e3d476b291
minh tam <tamlkp2@gmail.com>
2016-05-25 09:20:04 +0700

    update

--- javacore/java.lang/StringBuilder/src/StringBuilder/StringBuilderDemo.java
+++ javacore/java.lang/StringBuilder/src/StringBuilder/StringBuilderDemo.java
@@ -1,23 +1,24 @@
 package StringBuilder;
+
 import java.lang.*;
 
 public class StringBuilderDemo {
 
-   public static void main(String[] args) {
-  
-   StringBuilder str = new StringBuilder("India ");
-   System.out.println("string = " + str);
+	public static void main(String[] args) {
 
-   // append character to the StringBuilder
-   str.append('!');
-   // convert to string object and print it
-   System.out.println("After append = " + str.toString());
-      
-   str = new StringBuilder("Hi "); 
-   System.out.println("string = " + str);
-   // append integer to the StringBuilder
-   str.append(123);
-   // convert to string object and print it
-   System.out.println("After append = " + str.toString());
-   }
+		StringBuilder str = new StringBuilder("India ");
+		System.out.println("string = " + str);
+
+		// append character to the StringBuilder
+		str.append('!');
+		// convert to string object and print it
+		System.out.println("After append = " + str.toString());
+
+		str = new StringBuilder("Hi ");
+		System.out.println("string = " + str);
+		// append integer to the StringBuilder
+		str.append(123);
+		// convert to string object and print it
+		System.out.println("After append = " + str.toString());
+	}
 }
