e2577b352298b1aff1870b9435d03dc833b2caf5
minh tam <tamlkp2@gmail.com>
2016-05-31 11:54:57 +0700

    update mvcmodel

---
+++ java/MVCModel/studentmanagement/src/model/Student.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.sql.Date;
+
+public class Student {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private Date birthDate;
+	
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Student(int id, String firstName, String lastName, Date birthDate) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthDate = birthDate;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", birthDate=" + birthDate
+				+ "]";
+	}
+	
+	
+}
